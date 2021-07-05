05ff6ff29336a71a2696719d24c2b7316c4db316
Bhavya Sen <bhavyasen5@gmail.com>
2021-07-05 13:51:24 +0530

    want to change

--- icoder/settings.py
+++ icoder/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-7ofeg%w)psy2(*55)e5apg4pqupsf_#%0-o1v#g3ovd^(u@a#^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','firstinternblog.herokuapp.com']
 
 
 # Application definition
